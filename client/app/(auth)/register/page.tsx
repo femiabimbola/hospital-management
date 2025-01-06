5dfb59c5afec1ca13d176781c4a29355352e2010
femi abimbola <femiabimbola6@gmail.com>
2025-01-06 10:51:07 -0800

    added register form

--- client/app/(auth)/register/page.tsx
+++ client/app/(auth)/register/page.tsx
@@ -1,6 +1,13 @@
+import { RegisterForm } from "@/components/RegisterForm";
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
 const RegisterPage = () => {
  return ( 
-  <div> Register pager</div>
+    <RegisterForm />
+   
  )
 }
 
--- client/components/RegisterForm/index.tsx
+++ client/components/RegisterForm/index.tsx
@@ -1,6 +1,21 @@
 "use client"
 
+import { registerFormSchema } from "@/lib/formValidation/registerFormSchema"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
 export const RegisterForm = () => {
+
+  const form = useForm<z.infer<typeof registerFormSchema>>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  const submit = async() => {}
+
   return (
     <div> The register form</div>
   )
