ad72dbe25f40b9a70c25bc0a179d7b236d877567
femi abimbola <femiabimbola6@gmail.com>
2025-01-17 22:06:07 -0800

    started the model

--- server/src/controller/user.ts
+++ server/src/controller/user.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction, Router } from "express";
 import { validationResult, matchedData } from "express-validator";
+import bcrypt from "bcryptjs";
+import { getUserByEmail } from "../model/user";
+import { db } from "../lib/db";
 
 export const createUser = async (req: Request, res: Response) => {
   const result = validationResult(req);
@@ -7,4 +10,15 @@ export const createUser = async (req: Request, res: Response) => {
     return res.status(400).send({ error: result.array().map((err) => err) });
   }
   const data = matchedData(req);
+
+  const { fullName, email, password  } = data
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // const exisitingUser = await db.user.findUnique({ where: { email } });
+  const exisitingUser = await getUserByEmail(email);
+
+  if (exisitingUser) return res.status(400).send({ msg: "User exists" });
+
+  await db.user.create({ data: {  email, password: hashedPassword } });
 }
