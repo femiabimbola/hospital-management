3feebd1369a010f60d5bb2a05eebd74c3809c544
femi abimbola <femiabimbola6@gmail.com>
2025-03-17 15:46:36 -0700

    working on session

--- server/src/lib/mail/sendMail.ts
+++ server/src/lib/mail/sendMail.ts
@@ -6,6 +6,23 @@ dotenv.config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendMail = (req: Request, res: Response, next: NextFunction) => { 
+export const sendMail = async (req: Request, res: Response, next: NextFunction) => { 
+
+  // const confirmationLink = `${domain}/auth/new-verification?token=${token}`;
+
+  try {
+    const {data, error} = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: 'Hello World',
+      // html: `<p>  Click <a href="${confirmationLink}"> here </a> to confirm your email </p>`,
+      html: `<p>  Click to confirm your email </p>`,
+    });
+
+    res.status(200).json(data);
+  } catch(error) {
+    res.status(400).json(error);
+  }
+
   return res.status(400).send({ msg: "You do not have access for this route"})
 }
