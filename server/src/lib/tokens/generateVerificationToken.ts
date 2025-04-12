e6fb93077e2813bf59197648394155563d88e8a6
femi abimbola <femiabimbola6@gmail.com>
2025-04-12 04:39:59 -0700

    working on password reset

--- server/src/lib/tokens/generateVerificationToken.ts
+++ server/src/lib/tokens/generateVerificationToken.ts
@@ -40,3 +40,24 @@ export const generateVerificationToken = async (email: string) => {
 
   return verificationToken;
 };
+
+
+export const generatePasswordResetToken = async (email: string) => {
+
+  const token = uuid();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  // shows how to remove a value from database
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: {id: existingToken.id},
+    });
+  }
+  const verificationToken = await db.passwordResetToken.create({
+    data: {email, token, expires},
+  });
+
+  return verificationToken;
+};
