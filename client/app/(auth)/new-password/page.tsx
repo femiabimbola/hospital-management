d4fc64ccff239dd4b9d80cf54ddaa92c148c8a17
femi abimbola <femiabimbola6@gmail.com>
2025-04-13 09:10:59 -0700

    creating the reset page

--- client/app/(auth)/new-password/page.tsx
+++ client/app/(auth)/new-password/page.tsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+import { AuthNavbar } from "../AuthNavbar";
+import { ForgetPassword } from "@/components/ForgetPassword";
+
 const NewPasswordPage = () => {
-  return <div>
-    <p> The new password page</p>
-  </div>
+  return (
+    <div className="flex max-h-screen">
+      <section className="w-[60%] px-14 space-y-16">
+        <div>
+          <AuthNavbar />
+        </div>
+        <div className="pt-2">
+          <h1 className="w-full text-xl md:text-3xl font-bold text-center">
+            Enter new Password
+          </h1>
+        </div>
+          <ForgetPassword/>
+      </section>
+    
+      {/* The right side */}
+      <Image
+        src="/assets/login/doctor2.webp"
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="hidden h-full md:block max-w-[47%]"
+      />
+    </div>
+  )
 }
+
+export default NewPasswordPage;
--- server/src/lib/mail/sendMail.ts
+++ server/src/lib/mail/sendMail.ts
@@ -28,12 +28,12 @@ export const sendVerificationEmail = async ( email: string, token: string) => {
 
 export const ResetMail = async ( email: string, token: string) => { 
 
-  const PasswordResetLink = `${domain}/api/auth/reset?token=${token}`;
+  const PasswordResetLink = `${domain}/new-password?token=${token}`;
   try {
     const {data, error} = await resend.emails.send({
       from: 'femi J <hello@janennajiat50.com.ng>', 
       to: email,
-      subject: 'Reset your Passwprd',
+      subject: 'Reset your Password',
       html: `<p>  Click <a href="${PasswordResetLink}"> here </a> to confirm your email </p>`,
     });
 
